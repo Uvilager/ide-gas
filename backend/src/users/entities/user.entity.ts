8c1338681999d3eb0bfc92c3714d8b01c7811683
NikolicAleksandar <vojvodaaleksandar11@gmail.com>
2023-12-30 23:23:58 +0100

    branch publish, user entity

--- backend/src/users/entities/user.entity.ts
+++ backend/src/users/entities/user.entity.ts
@@ -1,1 +1,16 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  username: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+}
